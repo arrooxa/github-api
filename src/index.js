f9578883d2f8cb2a1983e09e6ad364521c22ce44
Vitor Rocha <vitor.league@gmail.com>
2022-02-18 15:40:46 -0300

    Versionando o boilerplate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './app'
+
+const RootElement = document.getElementById('root')
+
+ReactDOM.render(
+    <App />,
+    RootElement
+)
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.join(__dirname, '/public/index.html'),
+    })],
+};
